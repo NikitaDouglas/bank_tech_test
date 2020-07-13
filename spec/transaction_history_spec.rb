0b535bc00fb73df69e09c0cef38df2571f902a90
Nikita <latinisnotdead07@gmail.com>
2020-07-13 15:50:09 +0100

    #add_transaction also works for withdrawals

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -5,7 +5,8 @@ describe "TransactionHistory" do
 
   subject(:transaction_history) { TransactionHistory.new }
   let(:test_time) { Timecop.freeze(Time.local(2020, 7, 13)) }
-  let(:add_transaction) { transaction_history.add_transaction(test_time, 1000) } 
+  let(:add_transaction_deposit) { transaction_history.add_transaction(test_time, 1000) } 
+  let(:add_transaction_withdrawal) { transaction_history.add_transaction(test_time, -1000) }
 
   it "instantiates with an empty array" do
     expect(transaction_history.history).to eq []
@@ -13,7 +14,15 @@ describe "TransactionHistory" do
 
   describe "#add_transaction" do
     it "adds date and amount of deposit to transaction_history of account" do
-      expect{ add_transaction }.to change{ transaction_history.history }.to([{:date => test_time, :amount => 1000}])
+      expect{ add_transaction_deposit }
+      .to change{ transaction_history.history }
+      .to([{:date => test_time, :amount => 1000}])
+    end
+
+    it "adds date and amount of withdrawal to transaction_history of account" do
+      expect{ add_transaction_withdrawal }
+      .to change{ transaction_history.history }
+      .to([{:date => test_time, :amount => -1000}])
     end
   end
 end
