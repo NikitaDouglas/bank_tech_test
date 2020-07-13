090194fe1a9dc008ef76cf42931306094c1937c9
Nikita <latinisnotdead07@gmail.com>
2020-07-13 11:53:50 +0100

    removes commented out code

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -21,13 +21,3 @@ describe "Account" do
     end
   end
 end
-
-
-
-
-  #  it 'allows the shopper to see the price formatted as £XX.XX' do
-  #     [0.4, 5.6, 3.0].each do |price|
-  #       allow(item_double).to receive(:check_price) { price }
-  #       checkout.scan(item_double)
-  #     end
-  #     expect(checkout.see_total).to eq("£9.00")
