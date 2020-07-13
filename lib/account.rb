58f740737308e28b77d806686a04e80190710505
Nikita <latinisnotdead07@gmail.com>
2020-07-13 12:27:19 +0100

    refactors to use private method insuff_funds_message

--- lib/account.rb
+++ lib/account.rb
@@ -12,11 +12,17 @@ class Account
   end
 
   def withdraw(amount)
-    return "You don't have enough money in your account to withdraw that much." if insufficient_funds?(amount)
+    return insufficient_funds_message if insufficient_funds?(amount)
     @balance -= amount
   end
 
+  private
+
   def insufficient_funds?(amount)
     @balance - amount < DEFAULT_BALANCE
+  end
+
+  def insufficient_funds_message
+    "You don't have enough money in your account to withdraw that much."
   end
 end
