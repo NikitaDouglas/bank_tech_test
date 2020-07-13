2bb72dd831d8bce6ae8c84d7e5ec9016a2e9988f
Nikita <latinisnotdead07@gmail.com>
2020-07-13 15:46:05 +0100

    refactors Timecop.freeze

--- spec/transaction_history_spec.rb
+++ spec/transaction_history_spec.rb
@@ -4,7 +4,7 @@ require 'timecop'
 describe "TransactionHistory" do
 
   subject(:transaction_history) { TransactionHistory.new }
-  let(:test_time) { Time.local(2020, 7, 13) }
+  let(:test_time) { Timecop.freeze(Time.local(2020, 7, 13)) }
   let(:add_transaction) { transaction_history.add_transaction(test_time, 1000) } 
 
   it "instantiates with an empty array" do
@@ -12,8 +12,7 @@ describe "TransactionHistory" do
   end
 
   describe "#add_transaction" do
-    it "adds date and amount to transaction_history of account" do
-      Timecop.freeze(test_time)
+    it "adds date and amount of deposit to transaction_history of account" do
       expect{ add_transaction }.to change{ transaction_history.history }.to([{:date => test_time, :amount => 1000}])
     end
   end
