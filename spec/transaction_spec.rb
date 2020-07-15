8fa29a10e5fe26c350ba70c4e336cd12fc975762
Nikita <latinisnotdead07@gmail.com>
2020-07-15 11:20:59 +0100

    Rectifies Timecop error in transaction_spec.rb

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,9 +1,14 @@
 require "transaction"
+require "timecop"
 
 describe "Transaction" do
 
-  let(:test_time) { Timecop.freeze(Time.local(2020, 7, 13)) }
+  let(:test_time) { Time.local(2020, 7, 13) }
   subject(:transaction) { Transaction.new(1000, 0, 0) }
+
+  before do
+    Timecop.freeze(test_time)
+  end
 
   it "should instatiate with a date equal to Time.now by default" do
     expect(transaction.date).to eq test_time
