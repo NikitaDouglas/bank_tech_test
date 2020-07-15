ab1917345a8d70e94ee0428342c4835511e78c9d
Nikita <latinisnotdead07@gmail.com>
2020-07-15 10:39:33 +0100

    Refactors spec to remove duplicate test

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -5,12 +5,7 @@ describe "Transaction" do
   let(:test_time) { Timecop.freeze(Time.local(2020, 7, 13)) }
   subject(:transaction) { Transaction.new(test_time) }
 
-  it "should instantiate with a date" do
-    expect(transaction.date).to eq test_time
-  end
-
   it "should instatiate with a date equal to Time.now by default" do
-    new_transaction = Transaction.new
-    expect(new_transaction.date).to eq test_time
+    expect(transaction.date).to eq test_time
   end
 end
